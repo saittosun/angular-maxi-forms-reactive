83ac691a0443ee78ee1c9b8bb36cb6d1d9f56069
tostos <saittosun2@gmail.com>
2020-05-14 12:44:24 +0200

    doing custom validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,15 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['esat', 'ayla'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(
+          null,
+          [Validators.required, this.onForbiddenNames.bind(this)]
+        ),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -28,5 +32,14 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  onForbiddenNames(control: FormControl): {[s: string]: boolean} {// bu function buna donecek =>{nameIsForbidden: true}
+    // tslint:disable-next-line:max-line-length
+    // -1 is interpreted as true though. So we need to check if this is not equal to -1. So if this is not equal to -1, that means we did find it, that means it is invalid.
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
